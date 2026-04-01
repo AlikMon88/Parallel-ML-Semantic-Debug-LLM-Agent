c8ccba9ca280e6a4d0e99a4d3d7e3c635eef43e4
unknown <aliktrikemaf@gmail.com>
2026-04-01 16:00:12 +0530

    add vector-embd

--- rag/embed.py
+++ rag/embed.py
@@ -1,3 +1,9 @@
+import os
+import json
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+
 def create_vector_store(documents):
     # Using OpenAI's optimized embedding model
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
