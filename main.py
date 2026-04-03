9177b97edd99ba1ee3ce3fdd2d35784528e9a789
unknown <aliktrikemaf@gmail.com>
2026-04-03 14:35:46 +0530

    add routing

--- main.py
+++ main.py
@@ -47,20 +47,33 @@ def load_llm(model_name='other'):
 
 if __name__ == '__main__':
     
+    cate_dict = {
+        '[DATA]': 'data/incident_spaces/data_incidents.json',
+        '[COMPUTE]': 'data/incident_spaces/compute_incidents.json',
+        '[CODE]': 'data/incident_spaces/code_incidents.json'
+    }
+    vector_embed_cache = {}
     
-    documents = load_documents()
+    documents = load_documents(cate_dict['[DATA]'])
     
     ## Reference for RAG
+    print(' --- DATA-RAG-DOC --- ')
     pprint(documents)
     
-    vector_embed = create_vector_store(documents) ## BERT-based embedding-vector (encoder-architechture)
-    print('vector-embed-shape: ', vector_embed.index.ntotal, vector_embed.index.d)
+    for key in cate_dict.keys():
+        documents = load_documents(cate_dict[key])
+        vector_embed_cache[key] = create_vector_store(documents) ## BERT-based embedding-vector (encoder-architechture)
+        print('vector-embed-shape: ', vector_embed_cache[key].index.ntotal, vector_embed_cache[key].index.d)
         
     user_query = "My model's accuracy suddenly dropped after yesterday's deployment. What should I check?"
+    ## out-of-distrib.
+    # user_query = "My eyes are feverish yellow, not the model look one would have since yesterday. What should I check?"
+    user_query = "My model is suddenly facing memory botteleneck RAM after yesterday's test when it was perfect. What should I check?"
+    
     print(f"User Query: '{user_query}'\n")
     
     print('SIMILARITY-SEARCH')
-    docs = vector_embed.similarity_search(user_query, k=2)
+    docs = vector_embed_cache['[CODE]'].similarity_search(user_query, k=2)
     for d in docs:
         print(d.page_content)
         print("---")
@@ -68,23 +81,19 @@ if __name__ == '__main__':
     # sys.exit()
     
     ## Decoder LLM (TinyLLAMA)
-    llm = load_llm()    
+    llm = load_llm(model_name='openai')   
     
     pprint(llm)
     
     category = route_query(user_query, llm)
     print(f"Agent Router classified this as: {category}\n")
     
-    
-    # if category == "ml_debug":
-    #     rag_chain = build_rag_chain(ml_vector_store, llm)
-
-    # if category == "infra":
-    #     rag_chain = build_rag_chain(infra_vector_store, llm)
-    
-    
-    rag_chain = build_rag_chain(vector_embed, llm)
+    ## better to use a vector_embedd cache
+    rag_chain = build_rag_chain(vector_embed_cache[category], llm)
     response = rag_chain.invoke(user_query)
+    
     
     print("=== AI Diagnostic Report ===")
     print(response)
+    
+    ### Streamlit
